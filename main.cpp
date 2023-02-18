9fa9868e6f94cae9ddb9b179750f130cbb213011
maximalekseenko <max.alekseenko@gmail.com>
2023-02-18 17:51:16 +0300

    logs

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 
 
 int main(){
+
+    std::cout << "--------------------------------------" << std::endl;
+    std::cout << "---------- +++ TASK 001 +++ ----------" << std::endl;
+
     triangle triangles[3];
 
     // input
     double a, b, c;
     for (triangle& __triangle: triangles){
+        std::cout << "input 3 sides for a triangle at " << &__triangle << std::endl;
         while (!__triangle.exst_tr()){
             std::cin >> a >> b >> c;
             __triangle.set(a, b, c);
@@ -18,7 +23,13 @@ int main(){
     // output
     for (triangle& __triangle: triangles){
         __triangle.show();
-        std::cout << __triangle.perimetr() << std::endl;
-        std::cout << __triangle.square() << std::endl;
+        std::cout << " perimeter is " << __triangle.perimetr() << std::endl;
+        std::cout << " square is " << __triangle.square() << std::endl;
+        
     }
+
+
+
+    // std::cout << "--------------------------------------" << std::endl;
+    // std::cout << "---------- +++ TASK 002 +++ ----------" << std::endl;
 }
