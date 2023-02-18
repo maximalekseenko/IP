4d9dce7c90c53c7c021f3cc3c1c3bdcb9b440670
maximalekseenko <max.alekseenko@gmail.com>
2023-02-18 15:56:27 +0300

    triangle done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
+#include "triangle.h"
+
 
 int main(){
-    char name[255];
-    std::cin >> name;
-    std::cout << "Hello, " << name << std::endl;
+    triangle triangles[3];
+
+    // input
+    double a, b, c;
+    for (triangle& __triangle: triangles){
+        while (!__triangle.exst_tr()){
+            std::cin >> a >> b >> c;
+            __triangle.set(a, b, c);
+        }
+    }
+
+    // output
+    for (triangle& __triangle: triangles){
+        __triangle.show();
+        std::cout << __triangle.perimetr() << std::endl;
+        std::cout << __triangle.square() << std::endl;
+    }
 }
