c4645b30b1fb570161265a90c10efbcd7d60c000
maximalekseenko <max.alekseenko@gmail.com>
2023-02-26 13:50:25 +0300

    figure

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <vector>
 
 #include "triangle.h"
 #include "circle.h"
+#include "figure.h"
 
 
 int main(){
@@ -34,14 +36,16 @@ int main(){
     std::cout << "--------------------------------------" << std::endl;
     std::cout << "---------- +++ TASK 002 +++ ----------" << std::endl;
 
-    circle circles[3];
+    std::vector<circle> circles;
 
     // input
     double r, x, y;
-    for (circle& __circle : circles){
-        std::cout << "input radius, x and y for a circle at " << &__circle << std::endl;
+    for (int i = 0; i < 3; i ++){
+        circle _circle;
+        std::cout << "input radius, x and y for a circle at " << &_circle << std::endl;
         std::cin >> r >> x >> y;
-        __circle.set_circle(r, x, y);
+        _circle.set_circle(r, x, y);
+        circles.push_back(_circle);
     }
 
     // output
@@ -54,12 +58,39 @@ int main(){
             std::cout << "  around is " << _circle.triangle_around(_triangle.a, _triangle.b,c) << std::endl;
             std::cout << "  in is " << _circle.triangle_in(_triangle.a, _triangle.b,c) << std::endl;
         }
-        // for (circle& _circle : circles){
-        //     for (circle& _circle2 : circles){
-        //         if (&_circle == &_circle2) continue;
-        //         std::cout << " check_circle for circles at " << &_circle << " and " << &_circle2 << " is " << _circle.check_circle(&_circle.) << std::endl;
+        for (circle& _circle : circles){
+            for (circle& _circle2 : circles){
+                std::cout << &_circle << " " << &_circle2 << std::endl;
+                // std::cout << " check_circle for circles at " << &_circle << " and " << &_circle2 << " is " << _circle.check_circle(&_circle.) << std::endl;
                 
-        //     }
-        // }
+            }
+        }
+    }
+
+
+    std::cout << "--------------------------------------" << std::endl;
+    std::cout << "---------- +++ TASK 003 +++ ----------" << std::endl;
+
+    std::vector<figure> figures;
+
+    // input
+    double x1, x2, x3, x4, y1, y2, y3, y4;
+    for (int i = 0; i < 3; i ++){
+        std::cout << "input x1, x2, x3, x4, y1, y2, y3, y4 for a figure" << std::endl;
+        std::cin >> x1 >> x2 >> x3 >> x4 >> y1 >> y2 >> y3 >> y4;
+        figures.push_back(figure(x1, x2, x3, x4, y1, y2, y3, y4));
+    }
+
+    // output
+    for (figure& _figure : figures){
+        _figure.show();
+
+        std::cout << " prug " << _figure.is_prug() << std::endl;
+        std::cout << " square " << _figure.is_square() << std::endl;
+        std::cout << " romb " << _figure.is_romb() << std::endl;
+        std::cout << " in circle " << _figure.is_in_circle() << std::endl;
+        std::cout << " out circle " << _figure.is_out_circle() << std::endl;
     }
 }
+
+
